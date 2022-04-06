1fe60c2b2a5d009303e00d51b5ae259ffe2f701e
puneet2121 <bedipuneet21@gmail.com>
2022-04-05 21:23:14 -0600

    if else

--- conditional.js
+++ conditional.js
@@ -1,1 +1,9 @@
-console.log("heoolo")
+const raining = true;
+const cold = true;
+if (raining) {
+    console.log("please take umbrella with you");
+}
+if (cold){
+    console.log("please take a jacket with you")
+}
+raining
